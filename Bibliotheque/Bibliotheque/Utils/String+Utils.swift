fe3715875e346f88d6324b3cf748c37f92bcf6bf
Artem Marhaza <artemmrgz@gmail.com>
2023-04-20 12:16:47 +0200

    apply style to html string

--- Bibliotheque/Bibliotheque/Utils/String+Utils.swift
+++ Bibliotheque/Bibliotheque/Utils/String+Utils.swift
@@ -8,18 +8,6 @@
 import UIKit
 
 extension String {
-//    func htmlAttributedString() -> NSAttributedString? {
-//        guard let data = self.data(using: .utf8) else {
-//            return nil
-//        }
-//
-//        return try? NSAttributedString(
-//            data: data,
-//            options: [.documentType: NSAttributedString.DocumentType.html],
-//            documentAttributes: nil
-//        )
-//    }
-    
     func htmlAttributedString() -> NSAttributedString? {
         let htmlTemplate = """
         <!doctype html>

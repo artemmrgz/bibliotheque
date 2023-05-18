ce28ae8c80686cd68f8d209ddcd205e811c581f9
Artem Marhaza <artemmrgz@gmail.com>
2023-05-18 12:52:30 +0200

    refactoring

--- Bibliotheque/Bibliotheque/Base Components/BaseViewController.swift
+++ Bibliotheque/Bibliotheque/Base Components/BaseViewController.swift
@@ -9,26 +9,22 @@ import UIKit
 import SPAlert
 
 class BaseViewController: UIViewController {
-    lazy var alertView: SPAlertView = {
-        let alert = SPAlertView(title: "", message: "", preset: .done)
-        alert.titleLabel?.textColor = Resources.Color.textNavy
-        alert.subtitleLabel?.textColor = Resources.Color.textNavy
-        alert.iconView?.tintColor = Resources.Color.textNavy
-        alert.layout.iconSize = .init(width: 100, height: 100)
-        alert.layout.margins.top = 32
-        return alert
-    }()
     
     func displaySPAlert(title: String, message: String? = nil, preset: SPAlertIconPreset, haptic: SPAlertHaptic, completion: (() -> Void)? = nil) {
-
+        let alertView = SPAlertView(title: title, message: message, preset: preset)
+        
         if haptic == .error {
             alertView.dismissInTime = false
         }
-        
-        alertView.titleLabel?.text = title
-        alertView.subtitleLabel?.text = message
-        alertView.layout = SPAlertLayout.init(for: preset)
-        
+        styleAlertView(alertView)
         alertView.present(haptic: haptic, completion: completion)
+    }
+    
+    private func styleAlertView(_ alertView: SPAlertView) {
+        alertView.titleLabel?.textColor = Resources.Color.textNavy
+        alertView.subtitleLabel?.textColor = Resources.Color.textNavy
+        alertView.iconView?.tintColor = Resources.Color.textNavy
+        alertView.layout.iconSize = .init(width: 100, height: 100)
+        alertView.layout.margins.top = 32
     }
 }
